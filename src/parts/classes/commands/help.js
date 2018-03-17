4f736a4f19a6a451e0ddbdbba2ca2388143c609a
Joris Brandt <jorisbrandt@gmail.com>
2018-03-17 01:20:02 +0100

    Started actually making `help` do stuff

--- src/parts/classes/commands/help.js
+++ src/parts/classes/commands/help.js
@@ -4,6 +4,15 @@ class Help extends Ecommand {
         this._commandname = "help";
         this._desc = "Gives a list of commands or general information about them"
     }
+
+    run() {
+        
+        return true;
+    }
+
+    respond() {
+        return this.channel.send(this.reply);
+    }
 }
 
 commands.register(Help, ["help", "list", "commands"]);
