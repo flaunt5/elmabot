5c40a89822d79b8bc3a87b3e00511ab5ddc70d31
Joris Brandt <jorisbrandt@gmail.com>
2018-03-30 02:15:43 +0200

    modified some gulp stuff

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const gulp = require('gulp'),
     gutil = require('gulp-util'),
     concat = require('gulp-concat'),
     sourcemaps = require('gulp-sourcemaps'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify-es').default;
 
 gulp.task('default', function(){
     'use strict';
@@ -16,7 +16,16 @@ gulp.task('build', function () {
     return gulp.src(['src/config.js', 'src/parts/*.js', 'src/parts/classes/*.js', 'src/parts/classes/**/*.js', 'src/bot.js'])
         .pipe(sourcemaps.init())
         .pipe(concat('app.js'))
-        .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task("prod", function () {
+    return gulp.src(['src/config.js', 'src/parts/*.js', 'src/parts/classes/*.js', 'src/parts/classes/**/*.js', 'src/bot.js'])
+        .pipe(sourcemaps.init())
+        .pipe(concat('app.js'))
+        .pipe(uglify())
+        .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('./'));
 });
