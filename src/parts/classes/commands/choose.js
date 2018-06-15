a7ac089d7cb38057c021a105cbb944fce878065e
Joris Brandt <jorisbrandt@gmail.com>
2018-06-15 23:36:52 +0200

    final choose fix

--- src/parts/classes/commands/choose.js
+++ src/parts/classes/commands/choose.js
@@ -10,25 +10,26 @@ class Choose extends Ecommand {
         return new Promise((resolve, reject) => {
             let bits;
             try {
-                bits = this.message.content.match(new RegExp("^(" + this.prefix + "\\w+) (.+)", "gi"));
+                bits = this.message.content.match(new RegExp("^(" + this.prefix + "\\w+) (.+)", "i"));
             } catch (e) {
                 reject(e);
             }
 
             if(bits === null) {
-                this.reply = "I need options to pick from :(";
+                this.reply = "I need options to pick from 😖";
                 resolve(true);
             } else {
                 let string = bits[2];
                 console.log(string);
                 let list = string.split(new RegExp("[,;]", "m"));
-                list = list.filter(v=>v!='');
+                list = list.filter(v => v != '');
                 console.log(list);
-                if(list.length < 1) {
+                if (list.length < 1) {
                     this.reply = "I can't choose from empty options 😖"
+                } else if(list.length === 1) {
+                    this.reply = "You only gave me one option so I'm gonna say " + list[0];
                 } else {
-                    let num = Math.floor(Math.random() * (list.length - 1) +1);
-                    this.reply = list[num];
+                    this.reply = list[Math.floor(Math.random() * list.length)];
                 }
                 resolve(true);
             }
